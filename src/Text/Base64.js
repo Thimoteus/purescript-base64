a77ee75a99ff96af57182c2f20e7198a4c7ad395
thimoteus <thimoteus@teknik.io>
2016-01-09 09:55:12 -0800

    fixed FFI bug

--- src/Text/Base64.js
+++ src/Text/Base64.js
@@ -1,19 +1,21 @@
 // module Text.Base64
 
-if (typeof(btoa) == 'undefined') {
-  var btoa = null;
-  var atob = null;
-  exports.encode64 = function (str) {
+exports.encode64 = function (str) {
+  if (typeof(btoa) == 'undefined') {
+    var btoa = null;
+    var atob = null;
     return new Buffer(str).toString('base64');
-  }
-  exports.decode64 = function (code) {
-    return new Buffer(code, 'base64').toString('ascii');
-  }
-} else {
-  exports.encode64 = function (str) {
+  } else {
     return btoa(str);
   }
-  exports.decode64 = function (code) {
+}
+
+exports.decode64 = function (code) {
+  if (typeof(atob) == 'undefined') {
+    var btoa = null;
+    var atob = null;
+    return new Buffer(code, 'base64').toString('ascii');
+  } else {
     return atob(code);
   }
 }
