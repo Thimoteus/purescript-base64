857e5134aa398d864fe17a912f52fcb61ed8f037
Tomasz Rybarczyk <paluho@gmail.com>
2019-09-16 06:38:35 +0200

    Minor js tweaks: use typeof as operator + add missing semicolons

--- src/Text/Base64.js
+++ src/Text/Base64.js
@@ -1,17 +1,17 @@
 // module Text.Base64
 
 exports.encode64 = function (str) {
-  if (typeof(btoa) == 'undefined') {
+  if (typeof btoa == 'undefined') {
     return new Buffer(str).toString('base64');
   } else {
     return btoa(str);
   }
-}
+};
 
 exports.decode64 = function (code) {
-  if (typeof(atob) == 'undefined') {
+  if (typeof atob == 'undefined') {
     return new Buffer(code, 'base64').toString('ascii');
   } else {
     return atob(code);
   }
-}
+};
