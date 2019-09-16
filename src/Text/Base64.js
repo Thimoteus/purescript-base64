90516a5155a61fef3122eb5e3ae261a8b219b41c
Tomasz Rybarczyk <paluho@gmail.com>
2019-09-16 06:43:06 +0200

    Use `===` when comparing strings on js side.

--- src/Text/Base64.js
+++ src/Text/Base64.js
@@ -1,7 +1,7 @@
 // module Text.Base64
 
 exports.encode64 = function (str) {
-  if (typeof btoa == 'undefined') {
+  if (typeof btoa === 'undefined') {
     return new Buffer(str).toString('base64');
   } else {
     return btoa(str);
@@ -9,7 +9,7 @@ exports.encode64 = function (str) {
 };
 
 exports.decode64 = function (code) {
-  if (typeof atob == 'undefined') {
+  if (typeof atob === 'undefined') {
     return new Buffer(code, 'base64').toString('ascii');
   } else {
     return atob(code);
